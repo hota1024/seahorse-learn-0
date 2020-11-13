fa2a245cbe15e553e91b813d53f98eaf528d7712
hota1024 <hotalog@hota1024.com>
2020-11-13 22:56:26 +0900

    :sparkles: Write sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use seahorse::App;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("cli [args]")
+        .action(|c| println!("Hello, {:?}", c.args));
+
+    app.run(args);
 }
